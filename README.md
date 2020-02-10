619461611fe5c6e1cbaee2423227a477df3ba210
Nisheshg5 <46001977+Nisheshg5@users.noreply.github.com>
2020-02-10 22:41:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Samavesh WebSite
+ A website to provide your medical documents in case of emergencies according to your RFID tags
---
+++ js/app.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyD-cFgv83lcMeJqP3mpNypr9CyQ-U9qZkI",
+	authDomain: "rfid-bd012.firebaseapp.com",
+	databaseURL: "https://rfid-bd012.firebaseio.com",
+	projectId: "rfid-bd012",
+	storageBucket: "rfid-bd012.appspot.com",
+	messagingSenderId: "779567636963",
+	appId: "1:779567636963:web:9cd76f6e62297a49d400d6",
+	measurementId: "G-1R21VZFTHB"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
